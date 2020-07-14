6e17981996b437eccb9903fad722c589ee160298
Sam <samcparker1999@gmail.com>
2020-07-14 10:19:12 +0100

    Changed it to use local storage rather than cookies in some parts

--- components/Ingredients.vue
+++ components/Ingredients.vue
@@ -2,19 +2,21 @@
     <div>
         <h2>Ingredients</h2>
         <v-row class="flex-column pl-2">
-            <v-checkbox :value="values.indexOf(index) != -1 ? true : false" @change="toggleCheckbox(index, $event)" v-for="(ingredient, index) in ingredients" :key="index" style="padding: 0 0 0 0; margin: 0 0 0 0" color="#26a69a" class="mx-2" :label="ingredient.amount * multiplier + ' ' + ingredient.desc"></v-checkbox>
+            <!-- <v-checkbox :value="values.indexOf(index) != -1 ? true : false" @change="toggleCheckbox(index, $event)" v-for="(ingredient, index) in ingredients" :key="index" style="padding: 0 0 0 0; margin: 0 0 0 0" color="#26a69a" class="mx-2" :label="ingredient.amount * multiplier + ' ' + ingredient.desc"></v-checkbox> -->
+            <v-checkbox v-model="checked" v-for="(ingredient, index) in ingredients" :key="index" style="padding: 0 0 0 0; margin: 0 0 0 0" color="#26a69a" class="mx-2" :label="ingredient.amount * multiplier + ' ' + ingredient.desc"></v-checkbox>
         </v-row>
+        <p>{{ checked }}</p>
     </div>
 </template>
 
 <script>
-import VueCookies from 'vue-cookies';
 
 export default {
     data: () => {
         return {
             multiplier: 1,
-            values: []
+            values: [],
+            checked: false
         }
     },
     props: {
@@ -35,52 +37,56 @@ export default {
                 this.multiplier = 1;
             }
         },
-    doSelectedIngredients() {
-        var key = 'ingredients-' + this.$route.params.slug;
-        var selectedIngredientsStr = VueCookies.get(key);
-        if (selectedIngredientsStr == null) {
-            return;
-        }
-        this.values = JSON.parse(selectedIngredientsStr);
+    
+    },
+    watch: {
+
+    //     doSelectedIngredients() {
+    //     var key = 'ingredients-' + this.$route.params.slug;
+    //     var selectedIngredientsStr = this.$localStorage.getItem(key);
+    //     if (selectedIngredientsStr == null) {
+    //         return;
+    //     }
+    //     this.values = JSON.parse(selectedIngredientsStr);
 
 
-      },
-      toggleCheckbox(index, value) {
-          console.log("Values: " + this.values);
-          var key = 'ingredients-' + this.$route.params.slug;
+    //   },
+    //   toggleCheckbox(index, value) {
+    //       console.log("Values: " + this.values);
+    //       var key = 'ingredients-' + this.$route.params.slug;
           
-          var selectedIngredientsStr = VueCookies.get(key);
-            console.log(selectedIngredientsStr);
-          if (selectedIngredientsStr) {
-              var selectedIngredientsJSON = JSON.parse(selectedIngredientsStr);
+    //       var selectedIngredientsStr = this.$localStorage.getItem(key);
+    //         console.log(selectedIngredientsStr);
+    //       if (selectedIngredientsStr) {
+    //           var selectedIngredientsJSON = JSON.parse(selectedIngredientsStr);
 
-              if (value) {
-                  if (selectedIngredientsJSON.indexOf(index) == -1) {
-                    selectedIngredientsJSON.push(index);
-                  }
+    //           if (value) {
+    //               if (selectedIngredientsJSON.indexOf(index) == -1) {
+    //                 selectedIngredientsJSON.push(index);
+    //               }
                   
-              }
-              else {
-                selectedIngredientsJSON = selectedIngredientsJSON.filter((el) => el != index);
-              }
+    //           }
+    //           else {
+    //             selectedIngredientsJSON = selectedIngredientsJSON.filter((el) => el != index);
+    //           }
 
-              console.log(selectedIngredientsJSON);
-              this.values = selectedIngredientsJSON;
-            VueCookies.set(key, JSON.stringify(selectedIngredientsJSON));
-          }
-          else {
-              if (value) {
-                  var selectedIngredientsJSON = [index];
-                  console.log(selectedIngredientsJSON);
-                  this.values = selectedIngredientsJSON;
-                    VueCookies.set(key, JSON.stringify(selectedIngredientsJSON));
-              }
-          }
+    //           console.log(selectedIngredientsJSON);
+    //           this.values = selectedIngredientsJSON;
+    //         this.$localStorage.setItem(key, JSON.stringify(selectedIngredientsJSON));
+    //       }
+    //       else {
+    //           if (value) {
+    //               var selectedIngredientsJSON = [index];
+    //               console.log(selectedIngredientsJSON);
+    //               this.values = selectedIngredientsJSON;
+    //                 this.$localStorage.setItem(key, JSON.stringify(selectedIngredientsJSON));
+    //           }
+    //       }
          
-      },
+    //   },
     },
     mounted (){
-        this.doSelectedIngredients();
+        // this.doSelectedIngredients();
     }
 }
 </script>
--- components/Instructions.vue
+++ components/Instructions.vue
@@ -3,20 +3,20 @@
     <h2>Instructions
       <span >
       <v-btn-toggle mandatory v-model="toggle_exclusive">
-        <v-btn  v-for="(color, index) in colors" :key="index" @click="setFocusedColor(color)" small icon >
+        <v-btn v-for="(color, index) in colors" :key="index" @click="setFocusColor(color)" small icon >
           <v-icon :color="color">{{ color != "None" ? `mdi-square` : `mdi-square-off-outline` }}</v-icon>
         </v-btn>
       </v-btn-toggle>
       </span>
     </h2>
-    <div @click="setFocusedStep(step.step)" v-for="(step, index) in instructions" :key="index" :style="style(step.step)" class="mb-2 pa-2">
+    <!-- <div @click="setFocusedStep(step.step)" v-for="(step, index) in instructions" :key="index" :style="style(step.step)" class="mb-2 pa-2"> -->
+    <div @click="setFocusedStep(step.step)" v-for="(step, index) in instructions" :key="index" :style="pStyle(step.step)" class="mb-2 pa-2">
       <p><strong>{{ step.step }}.</strong> {{ step.desc }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import VueCookies from 'vue-cookies';
 
 export default {
     props: {
@@ -25,91 +25,103 @@ export default {
     data: () => {
       return {
         focusedStep: 1,
-        focusOn: true,
-        focusColor: "#f1c5c580",
+        focusColor: undefined,
         toggle_exclusive: undefined,
         colors: ["#f1c5c580", "#8bcdcd80", "#fa26a044", "None"]
       }
     },
     mounted () {
-      this.doFocusedStep();
-      this.doFocusedColor();
+      var focusColor = this.getFocusColor();
+      this.toggle_exclusive = this.colors.indexOf(focusColor);
+      this.focusedStep = this.getFocusedStep();
+    },
+    watch: {
       
     },
     methods: {
-      setFocusedStep(step) {
-        if (this.focusOn) {
-          this.focusedStep = step;
-          VueCookies.set('focus-' + this.$route.params.slug, step);
-        }
+      setFocusColor(focusColor) {
+        this.$localStorage.set("focusColor", focusColor);
       },
-      doSelectedIngredients() {
-
+      getFocusColor() {
+        return this.$localStorage.get("focusColor") || this.colors[0];
       },
-      setSelectedIngredients() {
-
+      setFocusedStep(stepNumber) {
+        if (this.getFocusColor() == "None") {
+          return;
+        }
+        this.$localStorage.set("focusedStep-" + this.$route.params.slug, stepNumber);
+        this.focusedStep = stepNumber;
       },
-      addSelectedIngredient(index) {
-        var key = 'ingredients-' + this.$route.params.slug;
-
-        var selectedIngredients = VueCookies.get(key);
-        if (selectedIngredients == null) {
-          VueCookies.set(key, [index]);
-        }
-        else { // cookie does exist
-          selectedIngredients.add(index);
-        }
+      getFocusedStep() {
+        return this.$localStorage.get("focusedStep-" +  this.$route.params.slug) || 1;
       },
-      removeSelectedIngredient(index) {
-
-      },
-      doFocusedStep() {
-        this.focusedStep = VueCookies.get('focus-' + this.$route.params.slug) || 1;
-      },
-      doFocusedColor() {
-        var fc = VueCookies.get('focusColor');
-        if (fc == "None") { // "None" colour is chosen so show final index and turn focus off
-          this.toggle_exclusive = this.colors.indexOf(fc);
-          this.focusOn = false;
-        }
-        else if (fc == null) { // No colour has been selected, so show default colour at index 0 and turn focus on
-          this.toggle_exclusive = 0;
-          this.focusOn = true;
-        }
-        else { // A colour has been selected so turn focus on and set focus colour to whatever
-          this.focusOn = true;
-          this.focusColor = fc;
-          this.toggle_exclusive = this.colors.indexOf(fc);
-        }
-      },
-      setFocusedColor(color) {
-        if (color == "None") {
-          this.focusOn = false;
-          VueCookies.set('focusColor', "None");
-        }
-        else {
-          this.focusColor = color;
-          this.focusOn = true;
-          VueCookies.set('focusColor', color);
-        }
-        
-      },
-      style(index) {
-        if (index == this.focusedStep && this.focusOn) {
-
-          return {
-            color: "black",
-            // boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.35)",
-            backgroundColor: this.focusColor,
-            transitionDuration: "1s",
+      pStyle(stepNumber) {
+        var original = {
+          color: "black",
+          transitionDuration: "1s"
+        };
+        if (this.$localStorage && this.focusedStep == stepNumber) {
+          if (this.getFocusColor() != "None") {
+            return Object.assign({}, original, {backgroundColor: this.getFocusColor()});
           }
         }
-        return {
-          color: "black",
-          transitionDuration: "1s",
-        }
+         return Object.assign({}, original, {});
       }
-    }
+      
+      // setFocusedStep(step) {
+      //   if (this.focusOn) {
+      //     this.focusedStep = step;
+
+      //     this.$localStorage.setItem('focus-' + this.$route.params.slug, step);
+      //   }
+      // },
+      // doFocusedStep() {
+      //   this.focusedStep = this.$localStorage.getItem('focus-' + this.$route.params.slug) || 1;
+      // },
+      // doFocusedColor() {
+      //   var fc = this.$localStorage.getItem('focusColor');
+      //   if (fc == "None") { // "None" colour is chosen so show final index and turn focus off
+      //     this.toggle_exclusive = this.colors.indexOf(fc);
+      //     this.focusOn = false;
+      //   }
+      //   else if (fc == null) { // No colour has been selected, so show default colour at index 0 and turn focus on
+      //     this.toggle_exclusive = 0;
+      //     this.focusOn = true;
+      //   }
+      //   else { // A colour has been selected so turn focus on and set focus colour to whatever
+      //     this.focusOn = true;
+      //     this.focusColor = fc;
+      //     this.toggle_exclusive = this.colors.indexOf(fc);
+      //   }
+      // },
+      // setFocusedColor(color) {
+      //   if (color == "None") {
+      //     this.focusOn = false;
+      //     this.$localStorage.setItem('focusColor', "None");
+      //   }
+      //   else {
+      //     this.focusColor = color;
+      //     this.focusOn = true;
+      //     this.$localStorage.setItem('focusColor', color);
+      //   }
+        
+      },
+      // style(index) {
+      //   if (index == this.focusedStep && this.focusOn) {
+
+      //     return {
+      //       color: "black",
+      //       // boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.35)",
+      //       backgroundColor: this.focusColor,
+      //       transitionDuration: "1s",
+      //     }
+      //   }
+      //   return {
+      //     color: "black",
+      //     transitionDuration: "1s",
+      //   }
+      // }
+    
     // computed: {
     //   style(index) {
     //     if (index == this.focus) {
