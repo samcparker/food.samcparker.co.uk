d4d6cb82ebe43e4c34606aa1186765555f7a1aef
Sam <samcparker1999@gmail.com>
2020-07-14 10:56:08 +0100

    Local storage remembers selected ingredients

--- components/Ingredients.vue
+++ components/Ingredients.vue
@@ -3,9 +3,8 @@
         <h2>Ingredients</h2>
         <v-row class="flex-column pl-2">
             <!-- <v-checkbox :value="values.indexOf(index) != -1 ? true : false" @change="toggleCheckbox(index, $event)" v-for="(ingredient, index) in ingredients" :key="index" style="padding: 0 0 0 0; margin: 0 0 0 0" color="#26a69a" class="mx-2" :label="ingredient.amount * multiplier + ' ' + ingredient.desc"></v-checkbox> -->
-            <v-checkbox v-model="checked" v-for="(ingredient, index) in ingredients" :key="index" style="padding: 0 0 0 0; margin: 0 0 0 0" color="#26a69a" class="mx-2" :label="ingredient.amount * multiplier + ' ' + ingredient.desc"></v-checkbox>
+            <v-checkbox :value="selectedIngredients.indexOf(index) == -1 ? false : true" @change="toggleIngredient(index, $event)" v-for="(ingredient, index) in ingredients" :key="index" style="padding: 0 0 0 0; margin: 0 0 0 0" color="#26a69a" class="mx-2" :label="ingredient.amount * multiplier + ' ' + ingredient.desc"></v-checkbox>
         </v-row>
-        <p>{{ checked }}</p>
     </div>
 </template>
 
@@ -15,7 +14,7 @@ export default {
     data: () => {
         return {
             multiplier: 1,
-            values: [],
+            selectedIngredients: [],
             checked: false
         }
     },
@@ -30,13 +29,50 @@ export default {
             return this.multiplier;
         }
     },
+    mounted() {
+        var key = 'ingredients-' + this.$route.params.slug;
+
+        this.selectedIngredients = this.$localStorage.get(key) || [];
+    },
     methods: {
-        increase(amount) {
-            this.multiplier = parseInt(this.multiplier) + amount;
-            if (this.multiplier < 1) {
-                this.multiplier = 1;
+        // increase(amount) {
+        //     this.multiplier = parseInt(this.multiplier) + amount;
+        //     if (this.multiplier < 1) {
+        //         this.multiplier = 1;
+        //     }
+        // },
+        toggleIngredient(index, on) {
+            var key = 'ingredients-' + this.$route.params.slug;
+
+            var selectedString = this.$localStorage.get(key);
+
+
+            if (selectedString) { // record exists
+                var selectedObject = JSON.parse(selectedString);
+                if (on && selectedObject.indexOf(index) == -1) {
+                    selectedObject.push(index); // add the index to the list
+                }
+                else if (!on) {
+                    selectedObject = selectedObject.filter(el => { return el != index }); // Remove all 'index's from the list
+                }
+                
+                // Remove value if length is 0 
+                if (selectedObject.length == 0) {
+                    this.$localStorage.remove(key);
+                }
+                else {
+                    this.$localStorage.set(key, JSON.stringify(selectedObject));
+                }
+                this.selectedIngredients = selectedObject;
             }
-        },
+            else { // record does not exist so create a new one
+                if (on) {
+                    var selectedObject = [index];
+                    this.$localStorage.set(key, JSON.stringify(selectedObject));
+                    this.selectedIngredients = selectedObject;
+                }                
+            }
+        }
     
     },
     watch: {
@@ -85,9 +121,7 @@ export default {
          
     //   },
     },
-    mounted (){
-        // this.doSelectedIngredients();
-    }
+
 }
 </script>
 
--- components/Instructions.vue
+++ components/Instructions.vue
@@ -19,123 +19,55 @@
 <script>
 
 export default {
-    props: {
-        instructions: Array
-    },
-    data: () => {
-      return {
-        focusedStep: 1,
-        focusColor: undefined,
-        toggle_exclusive: undefined,
-        colors: ["#f1c5c580", "#8bcdcd80", "#fa26a044", "None"]
-      }
-    },
-    mounted () {
-      var focusColor = this.getFocusColor();
-      this.toggle_exclusive = this.colors.indexOf(focusColor);
-      this.focusedStep = this.getFocusedStep();
-    },
-    watch: {
-      
-    },
-    methods: {
-      setFocusColor(focusColor) {
-        this.$localStorage.set("focusColor", focusColor);
-      },
-      getFocusColor() {
-        return this.$localStorage.get("focusColor") || this.colors[0];
-      },
-      setFocusedStep(stepNumber) {
-        if (this.getFocusColor() == "None") {
-          return;
-        }
-        this.$localStorage.set("focusedStep-" + this.$route.params.slug, stepNumber);
-        this.focusedStep = stepNumber;
-      },
-      getFocusedStep() {
-        return this.$localStorage.get("focusedStep-" +  this.$route.params.slug) || 1;
-      },
-      pStyle(stepNumber) {
-        var original = {
-          color: "black",
-          transitionDuration: "1s"
-        };
-        if (this.$localStorage && this.focusedStep == stepNumber) {
-          if (this.getFocusColor() != "None") {
-            return Object.assign({}, original, {backgroundColor: this.getFocusColor()});
-          }
-        }
-         return Object.assign({}, original, {});
-      }
-      
-      // setFocusedStep(step) {
-      //   if (this.focusOn) {
-      //     this.focusedStep = step;
-
-      //     this.$localStorage.setItem('focus-' + this.$route.params.slug, step);
-      //   }
-      // },
-      // doFocusedStep() {
-      //   this.focusedStep = this.$localStorage.getItem('focus-' + this.$route.params.slug) || 1;
-      // },
-      // doFocusedColor() {
-      //   var fc = this.$localStorage.getItem('focusColor');
-      //   if (fc == "None") { // "None" colour is chosen so show final index and turn focus off
-      //     this.toggle_exclusive = this.colors.indexOf(fc);
-      //     this.focusOn = false;
-      //   }
-      //   else if (fc == null) { // No colour has been selected, so show default colour at index 0 and turn focus on
-      //     this.toggle_exclusive = 0;
-      //     this.focusOn = true;
-      //   }
-      //   else { // A colour has been selected so turn focus on and set focus colour to whatever
-      //     this.focusOn = true;
-      //     this.focusColor = fc;
-      //     this.toggle_exclusive = this.colors.indexOf(fc);
-      //   }
-      // },
-      // setFocusedColor(color) {
-      //   if (color == "None") {
-      //     this.focusOn = false;
-      //     this.$localStorage.setItem('focusColor', "None");
-      //   }
-      //   else {
-      //     this.focusColor = color;
-      //     this.focusOn = true;
-      //     this.$localStorage.setItem('focusColor', color);
-      //   }
-        
-      },
-      // style(index) {
-      //   if (index == this.focusedStep && this.focusOn) {
-
-      //     return {
-      //       color: "black",
-      //       // boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.35)",
-      //       backgroundColor: this.focusColor,
-      //       transitionDuration: "1s",
-      //     }
-      //   }
-      //   return {
-      //     color: "black",
-      //     transitionDuration: "1s",
-      //   }
-      // }
+  props: {
+      instructions: Array
+  },
+  data: () => {
+    return {
+      focusedStep: 1,
+      focusColor: undefined,
+      toggle_exclusive: undefined,
+      colors: ["#f1c5c580", "#8bcdcd80", "#fa26a044", "None"]
+    }
+  },
+  mounted () {
+    var focusColor = this.getFocusColor();
+    this.toggle_exclusive = this.colors.indexOf(focusColor);
+    this.focusedStep = this.getFocusedStep();
+  },
+  watch: {
     
-    // computed: {
-    //   style(index) {
-    //     if (index == this.focus) {
-    //       // focused element
-    //       return {
-    //         color: "red"
-    //       }
-    //     }
-    //     return {
-    //       color: "black"
-    //     }
-
-    //   }
-    // }
+  },
+  methods: {
+    setFocusColor(focusColor) {
+      this.$localStorage.set("focusColor", focusColor);
+    },
+    getFocusColor() {
+      return this.$localStorage.get("focusColor") || this.colors[0];
+    },
+    setFocusedStep(stepNumber) {
+      if (this.getFocusColor() == "None") {
+        return;
+      }
+      this.$localStorage.set("focusedStep-" + this.$route.params.slug, stepNumber);
+      this.focusedStep = stepNumber;
+    },
+    getFocusedStep() {
+      return this.$localStorage.get("focusedStep-" +  this.$route.params.slug) || 1;
+    },
+    pStyle(stepNumber) {
+      var original = {
+        color: "black",
+        transitionDuration: "1s"
+      };
+      if (this.$localStorage && this.focusedStep == stepNumber) {
+        if (this.getFocusColor() != "None") {
+          return Object.assign({}, original, {backgroundColor: this.getFocusColor()});
+        }
+      }
+      return Object.assign({}, original, {});
+    }
+  }
 }
 </script>
 
